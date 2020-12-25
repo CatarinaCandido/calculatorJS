d478b7a817ba943864cb970583a0c632d1d20d0c
CatarinaCandido <72628746+CatarinaCandido@users.noreply.github.com>
2020-12-25 19:09:26 +0000

    calculator layout

---
+++ Calculator.js
@@ -0,0 +1,19 @@
+
+
+
+var num = function(number) {
+   var number = document.getElementById('screen').value += number
+}
+
+
+
+
+function sum() {
+
+    var value1 = document.getElementById('screen').value
+    console.log(value1)
+    document.getElementById('screen').value = ''
+
+
+
+}
