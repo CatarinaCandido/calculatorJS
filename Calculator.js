0225662167109bdc7129fb15de08a019663dff4e
CatarinaCandido <72628746+CatarinaCandido@users.noreply.github.com>
2020-12-26 00:29:22 +0000

    Calculator working
    Power Off and Reset - done
    Numbers - done
    Operators - done
    Screen - done
    Operations - Working

--- Calculator.js
+++ Calculator.js
@@ -1,19 +1,88 @@
 
+var num1;
+var result;
+var operationReady;
+var operation;
+var power = false;
 
+ function num (number) {
 
-var num = function(number) {
-   var number = document.getElementById('screen').value += number
+     if(operationReady !== true && power === true ){
+         result = document.getElementById('screen').value += number
+     } else if(operationReady ===true && power === true){
+         num1=document.getElementById('screen').value += number
+     }
 }
 
-
-
-
-function sum() {
-
-    var value1 = document.getElementById('screen').value
-    console.log(value1)
+function cleanScreen() {
     document.getElementById('screen').value = ''
+}
+
+function operationType(operator) {
+    operationReady= true;
+    cleanScreen();
+    operation = operator
+}
+
+function equal(){
+
+     switch (operation) {
+
+         case '+':
+
+             result = (parseInt(result)+parseInt(num1))
+             document.getElementById('screen').value = result
+             num1 = null;
+             break;
+
+         break;
+         case '/':
+             result = (parseInt(result)/parseInt(num1))
+             document.getElementById('screen').value = result
+             num1 = null;
+             break;
+
+         case '*':
+             result = (parseInt(result)*parseInt(num1))
+             document.getElementById('screen').value = result
+             num1 = null;
+             break;
+         case '-':
+             result = (parseInt(result)-parseInt(num1))
+             document.getElementById('screen').value = result
+             num1 = null;
+             break;
+     }
+
+     if(document.getElementById('screen').value === 'NaN'){
+         document.getElementById('screen').value = ' !Error!'
+     }
+
+}
+
+function powerOff() {
+
+     setTimeout(function () {
+         document.getElementById('screen').value = ''
+     },2000)
+
+    if(power == false){
+        document.getElementById('screen').value = ' Welcome!'
+        power = true
+
+    } else{
+        document.getElementById('screen').value = 'Goodbye!'
+        power = false
+
+    }
 
 
+}
 
+function resetCalculator() {
+
+     num1 = null;
+     result = null;
+     operationReady = null;
+     cleanScreen()
 }
